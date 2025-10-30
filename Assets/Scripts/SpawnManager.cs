9d8a3f6494880130ca42a04c062e85373ebd5476
Srishtivshetty <srishti.shetty@indivillage.co>
2025-10-30 16:44:16 +0530

    SpawnManager spawn Prefabs

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+//using System.Numerics;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] enemies;
+    public GameObject powerup;
+    private float zEnemySpawn = 12.0f;
+    private float xSpawnRange = 16.0f;
+    private float zPowerupRange = 5.0f;
+    private float ySpwan = 0.75f;
+    private float powerupSpawnTime = 5.0f;
+    private float enemySpawnTime = 1.0f;
+    private float startDelay = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomEnemy", startDelay, enemySpawnTime);
+        InvokeRepeating("SpawnPowerup", startDelay, powerupSpawnTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void SpawnRandomEnemy()
+    {
+        float randomX = Random.Range(-xSpawnRange, xSpawnRange);
+        int randomIndex = Random.Range(0, enemies.Length);
+        Vector3 spawnPos = new Vector3(randomX, ySpwan, zEnemySpawn);
+        Instantiate(enemies[randomIndex], spawnPos, enemies[randomIndex].gameObject.transform.rotation);
+
+    }
+    void SpawnPowerup()
+    {
+        float randomX = Random.Range(-xSpawnRange, xSpawnRange);
+        float randomZ = Random.Range(-zPowerupRange, zPowerupRange);
+        Vector2 spawnPos = new Vector3(randomX, ySpwan, zEnemySpawn);
+    }
+}
